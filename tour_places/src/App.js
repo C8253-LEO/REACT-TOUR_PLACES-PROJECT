7d58f9459989f8c4a11c0d7001ef24c1ac9b430d
LEO <yusufturkmen0748@gmail.com>
2021-08-13 17:18:53 +0300

    appteadet

--- tour_places/src/App.js
+++ tour_places/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Card from "../src/components/cards/Card";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import {places} from "./helper/data";
 
 function App() {
   return (
     <div className="App">
- 
+      <Navbar/>
+      <Header/>
+      {places.map(x =>(<Card placeName={x.placeName} placeImgSrc={x.placeImg} placeText={x.placeText}></Card>))}
     </div>
   );
 }
