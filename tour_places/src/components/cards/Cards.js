460adf3a8bde4cf50ffa8897fee6094bc323a1cd
LEO <yusufturkmen0748@gmail.com>
2021-08-13 16:36:40 +0300

    props created

---
+++ tour_places/src/components/cards/Cards.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Card.module.css";
+
+function Card(){
+    return(
+        <div className={styles["card-frame"]}>
+            <h2 className={styles.placeHeader}
+        </div>
+    )
+}
