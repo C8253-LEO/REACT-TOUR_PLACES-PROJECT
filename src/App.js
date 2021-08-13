04565a13018d368ac76a6d99d2cf05291532ed47
LEO <yusufturkmen0748@gmail.com>
2021-08-13 17:57:50 +0300

    files updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Card from "../src/components/cards/Card";
+import Card from "./components/cards/Card";
 import Navbar from "./components/navbar/Navbar";
 import Header from "./components/header/Header";
 import {places} from "./helper/data";
--- src/helper/data.js
+++ src/helper/data.js
@@ -15,71 +15,71 @@ export const places =[
     placeName:"Palawan, Philippines",
     placeImg:"https://www.planetware.com/photos-large/SEY/best-islands-palawan.jpg",
     placeText:"Palawan is the Philippine's answer to paradise. This island province stretches southwest to Borneo, with lush limestone peaks rising from a jewel-like sea so clear, that you can almost see the expressions on the fish from above the surface.",
-}
+},
 {
     placeName:"Seychelles",
     placeImg:"https://www.planetware.com/photos-large/SEY/best-islands-seychelles.jpg",
     placeText:"Almost half the total land area of these equatorial isles is protected, and many of the islands lie within fish-rich marine sanctuaries with excellent diving and snorkeling.",
-}
+},
 {
     placeName:"Santorini, Greece",
     placeImg:"https://www.planetware.com/photos-large/SEY/best-islands-santorini.jpg",
     placeText:"Encircling a sea-filled caldera, spectacular Santorini scores top points for its dramatic beauty. Bleached white villas tumble down volcanic cliffs. Blue-domed churches rise against the sparkling sea, and bright sprays of bougainvillea add to the eye-popping palette of color.",
-}
+},
 {
     placeName:"The Cook Islands",
     placeImg:"https://www.planetware.com/photos-large/SEY/best-islands-cook-islands.jpg",
     placeText:"If you've ever dreamed of being a castaway in the South Pacific, the Cook Islands are for you. Strung between French Polynesia and Samoa but with strong ties to New Zealand, the archipelago's 15 islands are among the most beautiful islands in the South Pacific.",
-}
+},
 {
     placeName:"Bali, Indonesia",
     placeImg:"https://www.planetware.com/photos-large/SEY/best-islands-bali.jpg",
     placeText:"Bali's intoxicating culture is a feast for the senses. Incense wafts from Hindu temples, rice paddies glow in electric greens, and the food jolts the taste buds.",
-}
+},
 {
     placeName:"The Dalmatian Islands, Croatia",
     placeImg:"https://www.planetware.com/photos-large/SEY/best-islands-dalmatian-islands.jpg",
     placeText:"In the Adriatic sea off the coast of Croatia, the Dalmatian Islands are an enchanting mix of relaxed charm and rich history. Quaint villages and boutique hotels and restaurants dot these gorgeous isles,",
-}
+},
 {
     placeName:"Fiji",
     placeImg:"https://www.planetware.com/photos-large/SEY/best-islands-fiji.jpg",
     placeText:"One of Australia's go-to tropical escapes, Fiji ticks all the fantasy island boxes. White-sand beaches, translucent turquoise waters, and teeming coral reefs are all found here in abundance. And the Fijian people welcome visitors with warm, friendly smiles.",
-}
+},
 {
     placeName:"Kaua'i",
     placeImg:"https://www.planetware.com/photos-large/SEY/best-islands-kauai.jpg",
-    placeText:"Kaua'i is called "the Garden Isle" for good reason: rainforests cloak much of the interior, waterfalls tumble from lava cliffs, and the intoxicating fragrance of tropical blooms hangs heavy on the sultry air.",
-}
+    placeText:"Kaua'i is called the Garden Isle for good reason: rainforests cloak much of the interior, waterfalls tumble from lava cliffs, and the intoxicating fragrance of tropical blooms hangs heavy on the sultry air.",
+},
 {
     placeName:"St. Lucia",
     placeImg:"https://www.planetware.com/photos-large/SEY/best-islands-st-lucia.jpg",
     placeText:"St. Lucia exudes a dark and brooding beauty. Unlike other Caribbean islands, its best assets are not necessarily its beaches, though with their golden sands and graceful palms, they are popular with those who visit.",
-}
+},
 {
     placeName:"Capri, Italy",
     placeImg:"https://www.planetware.com/photos-large/SEY/best-islands-capri.jpg",
     placeText:"Glamorous Capri, about 20 kilometers off the Amalfi Coast, lives up to all the hype. Sea stacks and soaring cliffs rise from the teal-hued Tyrrhenian Sea, framed by sprays of colorful flowers.",
-}
+},
 {
     placeName:"Ko Phi Phi, Thailand",
     placeImg:"https://www.planetware.com/photos-large/SEY/best-islands-ko-phi-phi.jpg",
     placeText:"In Ko Phi Phi, a small archipelago in the Krabi Province of Thailand, lush limestone peaks rise from the tropical sea, and warm waters lap on",
-}
+},
 {
     placeName:"Whitsunday Islands, Australia",
     placeImg:"https://www.planetware.com/wpimages/2020/04/world-most-beautiful-islands-whitsunday-islands-australia.jpg",
     placeText:"Sprinkled along the World Heritage-listed Great Barrier Reef, the Whitsunday Islands are Australia's version of the classic tropical island fantasy. These 74 lush islands are the peaks of drowned hills rising above the strikingly blue Coral Sea.",
-}
+},
 {
     placeName:"Sardinia, Italy",
     placeImg:"https://www.planetware.com/photos-large/SEY/best-islands-sardinia.jpg",
     placeText:"Blissful white-sand beaches, rugged coastal peaks, mouthwatering cuisine, a unique blend of cultures, and ancient archeological treasures: What's not to love about this saucy Italian isle?",
-}
+},
 {
     placeName:"St. Barts",
     placeImg:"https://www.planetware.com/photos-large/SEY/best-islands-st-barts.jpg",
     placeText:"Stunningly beautiful, yet understated, St. Barts lures movie stars and moguls from around the world, and for good reason. This pretty island offers a luscious taste of France in the tropics, with a relaxed charm that discerning travelers love.",
-}
+},
 
 ];
